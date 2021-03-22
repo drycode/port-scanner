00a20f81fc91e00b9848e2cf20df4f6acb747c99
Daniel Young <danyoungmusic93@gmail.com>
2021-03-21 20:03:52 -0400

    cleanup and reorganize packages for easier installation

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -1,4 +1,4 @@
-package portscanner
+package utils
 
 import (
 	"fmt"
@@ -6,18 +6,19 @@ import (
 	"sync"
 	"testing"
 )
+
 func TestSetPercentage(t *testing.T) {
 	ss := SafeSlice{}
 	var wg sync.WaitGroup
-	for i:=0; i < 1000; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go ss.append(strconv.Itoa(i), &wg)
 	}
 	wg.Wait()
-	
+
 	if ss.length() != 1000 {
 		fmt.Print(len(ss.OpenPorts))
 		t.Fail()
 	}
-	
+
 }

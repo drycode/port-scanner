04e7092d5bda9fe97a607b59ced97a51badd9ba6
Daniel Young <danyoungmusic93@gmail.com>
2021-04-15 17:22:33 -0400

    updated tests

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -1,10 +1,12 @@
-package utils
+package tests
 
 import (
 	"fmt"
 	"strconv"
 	"sync"
 	"testing"
+
+	. "github.com/drypycode/portscanner/utils"
 )
 
 func TestSafeSlice(t *testing.T) {
@@ -12,11 +14,11 @@ func TestSafeSlice(t *testing.T) {
 	var wg sync.WaitGroup
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
-		go ss.append(strconv.Itoa(i), &wg)
+		go ss.Append(strconv.Itoa(i), &wg)
 	}
 	wg.Wait()
 
-	if ss.length() != 1000 {
+	if ss.Length() != 1000 {
 		fmt.Print(len(ss.OpenPorts))
 		t.Fail()
 	}
